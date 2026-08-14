1d7c2afbedce2eec96346426ab6e7c7da794b4bd
FazezKurt23 <kurtjohnheraldez@gmail.com>
2026-08-13 17:36:54 -0800

    first commit

---
+++ constants/colors.ts
@@ -0,0 +1,12 @@
+export const COLORS = {
+  primary: 'rgba(118, 22, 163, 0.58)',
+  background: '#F5F9FF',
+  card: '#FFFFFF',
+  textPrimary: '#0D1B2A',
+  textSecondary: '#546E7A',
+  textOnPrimary: '#FFFFFF',
+  surface: '#E3F2FD',
+  border: '#E0E8F0',
+  shadow: '#0D47A1',
+  logo: 'rgb(129, 14, 158)'
+} as const;
